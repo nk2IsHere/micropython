86d2d8ab7e008dd3fd4cce52bb2eebcdba5d8fc5
nk2IsHere <nikkoz893@gmail.com>
2019-05-15 15:24:35 -0700

    [mod] add i2c module into build

--- ports/gprs_a9/mpconfigport.h
+++ ports/gprs_a9/mpconfigport.h
@@ -202,6 +202,7 @@ extern const struct _mp_obj_module_t chip_module;
 extern const struct _mp_obj_module_t cellular_module;
 extern const struct _mp_obj_module_t gps_module;
 extern const struct _mp_obj_module_t usocket_module;
+extern const struct _mp_obj_module_t i2c_module;
 
 #define MICROPY_PORT_BUILTIN_MODULES \
     { MP_OBJ_NEW_QSTR(MP_QSTR_machine), (mp_obj_t)&mp_module_machine }, \
@@ -211,6 +212,7 @@ extern const struct _mp_obj_module_t usocket_module;
     { MP_OBJ_NEW_QSTR(MP_QSTR_cellular), (mp_obj_t)&cellular_module }, \
     { MP_OBJ_NEW_QSTR(MP_QSTR_gps), (mp_obj_t)&gps_module }, \
     { MP_OBJ_NEW_QSTR(MP_QSTR_usocket), (mp_obj_t)&usocket_module }, \
+    { MP_OBJ_NEW_QSTR(MP_QSTR_i2c), (mp_obj_t)&i2c_module }, \
 
 #define MICROPY_PORT_BUILTIN_MODULE_WEAK_LINKS \
     { MP_OBJ_NEW_QSTR(MP_QSTR_binascii), (mp_obj_t)&mp_module_ubinascii }, \
